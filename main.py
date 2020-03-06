4e390671cedf74b86bdd2485a8049d77d6fb7763
Darcy382 <kyledarcy24@icloud.com>
2020-03-05 20:09:47 -0500

    Updating comments

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ from api_keys import get_google_api
 app = Flask(__name__)
 
 
-@app.route('/')  # Restaurant main page
+# Restaurant main page
+@app.route('/')
 def index():
     return render_template('search.html', results=search)
 
 
-@app.route("/search")  # Restaurant search
+# Restaurant search function
+@app.route("/search")
 def search():
     search_args = request.args  # Search arguments
     location = {
@@ -18,30 +20,32 @@ def search():
         'long': search_args['long']
     }
     # Gets search results from yelp's api
-    results = search_business(search_args['search_value'], search_args['lat'], search_args['long'])
+    businesses = search_business(search_args['search_value'], search_args['lat'], search_args['long'])
     # Calls the search_results function to render the results page
-    if len(results['businesses']) == 0:
-        return search_results(results, location, no_results=True)
+    if len(businesses) == 0:
+        return search_results(businesses, location, no_results=True)
     else:
-        biz_locations = get_biz_locations(results["businesses"])
-        return search_results(results, location, biz_locations, search_value=search_args['search_value'])
+        biz_locations = get_biz_locations(businesses)
+        return search_results(businesses, location, biz_locations, search_value=search_args['search_value'])
 
 
-@app.route('/results')  # Returns search results page
-def search_results(results, location, biz_locations=None, no_results=False, search_value=""):
-    return render_template("results.html", search_value=search_value, results=results, location=location,
+# Returns search results page
+@app.route('/results')
+def search_results(businesses, location, biz_locations=[], no_results=False, search_value=""):
+    return render_template("results.html", search_value=search_value, businesses=businesses, location=location,
                            biz_locations=biz_locations, no_results=no_results, google_api_key=get_google_api())
 
-# LEFT OFF COMMENTING HERE
+
+# Function displays a slides show of food pictures from nearby restaurants, when the user find a food that they like
+# they have the option to either go the that specific restaurant's yelp page, or search for similar food
 @app.route('/show_food')
 def show_food():
     search_args = request.args
-    results = search_business("Restaurants", search_args['lat'], search_args['long'], limit=50)
-    photo_urls = [biz["image_url"] for biz in results["businesses"]]
-    yelp_urls = [biz["url"] for biz in results["businesses"]]
-    categories = [biz["categories"][0]['title'] for biz in results["businesses"]]
-    return render_template('view_food.html', categories= categories,
-                           results=results, photo_urls=photo_urls, yelp_urls=yelp_urls)
+    businesses = search_business("Restaurants", search_args['lat'], search_args['long'], limit=50)
+    photo_urls = [biz["image_url"] for biz in businesses]
+    yelp_urls = [biz["url"] for biz in businesses]
+    categories = [biz["categories"][0]['title'] for biz in businesses]
+    return render_template('view_food.html', categories= categories, photo_urls=photo_urls, yelp_urls=yelp_urls)
 
 
 if __name__ == '__main__':
--- yelp_api.py
+++ yelp_api.py
@@ -9,16 +9,25 @@ ENDPOINT = "https://api.yelp.com/v3/businesses/search"
 HEADERS = {"Authorization": "bearer {}".format(API_KEY)}
 
 
+# Recursive function that gets bossiness data from the yelp fusion api.  If less than 7 results are found within 10km,
+# the function will resend the get request with a new radius of + 5km, the recursion process will repeat until
+# the radius is greater than 35km
 def search_business(term, lat, long, limit=10, radius=10000):
     parameters = {'radius': radius, 'term': term, "latitude": lat, "longitude": long, "limit": limit}
     response = requests.get(url=ENDPOINT, params=parameters, headers=HEADERS)
-    business_data = response.json()  # Convert JSON string to a Dict
-    return business_data
+    businesses = response.json()['businesses']  # Convert JSON string to a Dict
+    if radius > 35000:
+        return businesses
+    elif len(businesses) < 7:
+        return search_business(term, lat, long, limit=10, radius=radius + 5000)
+    else:
+        return businesses
 
 
+# Takes business search results and returns a list of just the business locations
 def get_biz_locations(business):
     business_locations = []
     for business in business:
-        business_locations.append([business['name'], business['coordinates']['latitude'], business['coordinates']['longitude']])
+        business_locations.append([business['name'], business['coordinates']['latitude'],
+                                   business['coordinates']['longitude']])
     return business_locations
-
